2c5e53bf6bf9b8de4151aec95ab9ae6abb6abefc
xiangdeshan <xiangdeshan_120@sina.com>
2018-01-18 12:34:57 +0800

    first
    整理

---
+++ js/static.js
@@ -0,0 +1,8 @@
+
+
+var urlxds ="http://192.168.1.2:8080/GFX/gfx/";
+var url = "http://192.168.1.2:8080/WMS_GFX/";
+var vacurl="http://192.168.1.2:8080/vackb/service/";
+/*var urlxds ="http://192.168.1.20:8888/GFX/gfx/";
+var url = "http://192.168.1.20:8888/WMS_GFX/";
+var vacurl="http://eqms-web.chinacloudapp.cn:10000/VAC/service/";*/
